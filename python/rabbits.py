f6bef7ba210678ca913d800ecb716aad6e15611e
frankts_chen <jcfrank7@gmail.com>
2014-01-27 18:08:55 +0800

    python version.

---
+++ python/rabbits.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3.3
+
+class Rabbit(object):
+    def __init__(self):
+        self._age = 0
+
+    @property
+    def age(self):
+        return self._age
+
+    def grow(self):
+        self._age += 1
+
+    def is_adult(self):
+        return self._age >= 3
+
+def count_rabbits(months):
+    r1 = Rabbit()
+    r2 = Rabbit()
+    rabbits = (r1, r2)
+    
+    for month in range(1, months + 1):
+        pair = 0
+        for rabbit in rabbits:
+            rabbit.grow()
+            if not rabbit.is_adult():
+                continue
+            pair += 1
+            if pair >= 2:
+                rabbits = rabbits + (Rabbit(), Rabbit())
+                pair = 0
+
+    return len(rabbits)
+
+# Main
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 2:
+        print("Please give the number of months.")
+        exit()
+    months = int(sys.argv[1])
+    print("Number of rabbits: %d" % count_rabbits(months))
+
